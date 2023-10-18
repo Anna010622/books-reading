ed7588b605b4571fd038d1e0855e9eb4d556e3f6
Anna Trifonova <3809322999@ukr.net>
2023-10-18 20:29:34 +0300

    Add global styles

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	plugins: [react()],
 	base: '/books-reading/',
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@import "./src/styles/_shared.scss";',
+			},
+		},
+	},
 });
