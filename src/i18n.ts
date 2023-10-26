96344fc1dc821b0ca462ce5de68febb98216acd7
Anna Trifonova <3809322999@ukr.net>
2023-10-26 19:06:04 +0300

    fix i18n settings

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,11 +4,24 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import translationEN from '../public/locales/en/translation.json';
+import translationUK from '../public/locales/uk/translation.json';
+
+const resources = {
+	en: {
+		translation: translationEN,
+	},
+	uk: {
+		translation: translationUK,
+	},
+};
+
 i18n
 	.use(Backend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
+		resources,
 		fallbackLng: 'uk',
 		debug: true,
 
