820be2059c840bb43b3f9c1429d19c3c9c57b148
Anna010622 <Anna010622@users.noreply.github.com>
2023-10-26 15:26:20 +0000

    deploy: 371f05a636647fad336633063d184b029a907d70

--- assets/HomePage-cd48c2de.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as e}from"./index-d009bcc9.js";const t="_pageContainer_153d6_1",s={pageContainer:t},r=()=>{const{t:n}=a();return e.jsxs("div",{className:`container ${s.pageContainer}`,children:[e.jsx("p",{children:"HomePage"}),e.jsx("p",{children:n("Welcome to React")})]})};export{r as default};
