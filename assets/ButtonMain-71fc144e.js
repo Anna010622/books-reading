16cded7b2fddf8faf7b35b76f1c684a422c8fb5c
Anna010622 <Anna010622@users.noreply.github.com>
2023-10-26 15:17:40 +0000

    deploy: 974976e68fbbe30a9f619e52179f023d6cc18dae

--- assets/ButtonMain-71fc144e.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-8e7ea14d.js";const r="_btn_1f9nr_1",b={btn:r},a=({title:t,onClick:n,type:s="button"})=>o.jsx("button",{className:b.btn,type:s,onClick:n,children:t});export{a as B};
--- assets/HomePage-12c29e83.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-8e7ea14d.js";const o=()=>e.jsx("div",{className:"pageContainer",children:"HomePage"});export{o as default};
