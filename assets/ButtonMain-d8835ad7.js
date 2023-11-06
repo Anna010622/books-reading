2bc35f1e09c4bb70ab2c5afc383ae46e544bb3cd
Anna010622 <Anna010622@users.noreply.github.com>
2023-11-06 20:22:54 +0000

    deploy: 6188d36ca6fbf89013f5c8f5635b4322a90490bf

--- assets/ButtonMain-d8835ad7.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-5fa9a2fa.js";const r="_btn_1f9nr_1",b={btn:r},a=({title:t,onClick:n,type:s="button"})=>o.jsx("button",{className:b.btn,type:s,onClick:n,children:t});export{a as B};
