8f010f4a1d4b5dfd517dfe8afb55f5a05b4fd406
Anna010622 <Anna010622@users.noreply.github.com>
2023-12-19 10:10:55 +0000

    deploy: 2bc0374fae3ec44298cc10d260f9c4239d149032

---
+++ assets/ButtonMain-f4b433ed.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-19e9215d.js";const o="_btn_1f9nr_1",r={btn:o},b=({children:t,...n})=>s.jsx("button",{className:r.btn,...n,children:t});export{b as B};
---
+++ assets/OutlineButton-42dff846.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-19e9215d.js";const b="/books-reading/assets/arrow-back-bb313d45.svg",o="_btn_1y9eh_1",r={btn:o},e=({children:t,...s})=>n.jsx("button",{className:r.btn,...s,children:t});export{e as O,b as i};
